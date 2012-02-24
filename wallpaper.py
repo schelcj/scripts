c796887a97dfe8226de677b9c71d09cf549ca1b3
Chris Scheller <schelcj@pobox.com>
2012-02-24 10:07:10 -0500

    still sorting things out

--- wallpaper.py
+++ wallpaper.py
@@ -5,94 +5,69 @@ import os
 
 class WallPaper(object):
   def __init__(self):
-    self._prefix           = os.getenv('HOME') + '/.wallpapers2/'
-    self._lock             = self._prefix + 'lock'
-    self._hist             = self._prefix + 'history.db'
-    self._category         = self._prefix + 'category'
-    self._wallpaper_dir    = self._prefix + 'Wallpapers'
-    self._current          = self._prefix + 'current'
-    self._resolution       = self._prefix + 'resolution'
-    self._log              = self._prefix + 'log'
-    self._sources          = self._prefix + 'sources'
-    self._bgsetter         = 'fbsetbg -a'
-    self._default_category = 'all'
+    self._prefix             = os.path.join(os.getenv('HOME'), '.wallpapers')
+    self._lock               = os.path.join(self._prefix, 'lock')
+    self._hist               = os.path.join(self._prefix, 'history.db')
+    self._category           = os.path.join(self._prefix, 'category')
+    self._wallpaper_dir      = os.path.join(self._prefix, 'Wallpapers')
+    self._current            = os.path.join(self._prefix, 'current')
+    self._resolution         = os.path.join(self._prefix, 'resolution')
+    self._log                = os.path.join(self._prefix, 'log')
+    self._sources            = os.path.join(self._prefix, 'sources')
+    self._bgsetter           = 'fbsetbg -a'
+    self._default_category   = 'all'
+    self._default_resolution = ''
+    self.sources             = []
+    self.wallpapers          = []
 
-    self.category   = self._get_category()
-    self.resolution = self._get_resolution()
-    self.sources    = self._get_source_dirs()
-    self.wallpapers = self._get_wallpapers()
+    self._parse_args()
+    self._build_categories()
+    self._build_resolution()
+    self._build_source_dirs()
+    self._build_wallpapers()
 
-  def __process_args(self):
-    if self.args.dump_cache:
-      self.dump_cache()
-      exit()
-
-    elif self.args.flush_cache:
-      self.flush_cache()
-      exit()
-
-    elif self.args.clear:
-      self.clear()
-
-    elif self.args.unlock:
-      self.unlock()
-      exit()
-
-    elif self.args.lock:
-      self.lock()
-      exit()
-
-    elif self.is_locked and not self.args.unlock:
-      print "currently locked, bailing out"
-      exit()
-
-    elif self.args.previous:
-      self.set_previous()
-      exit()
-
-  def _get_category(self):
-    category = self._default_category
-
+  def _build_categories(self):
     if self.args.category:
-      category = self.args.category
+      self.category = self.args.category
+
     elif os.path.exists(self._category):
-      category = file(self._category).read()
+      self.category = file(self._category).read()
 
-    return category
+    else:
+      self.category = self._default_category
 
-  def _get_resolution(self):
-    resolution = ''
+    with open(self._category, 'w') as f:
+      f.write(self.category)
 
+  def _build_resolution(self):
     if self.args.resolution:
-      resolution = self.args.resolution
+      self.resolution = self.args.resolution
+
     elif os.path.exists(self._resolution):
-      resolution = file(self._resolution).read()
+      self.resolution = file(self._resolution).read()
 
-    return resolution
+    else:
+      self.resolution = self._default_resolution
 
+    if self.resolution != self._default_resolution:
+      with open(self._resolution, 'w') as f:
+        f.write(self.resolution)
 
-  def _get_source_dirs(self):
-    dirs = []
-
-    if self.category == 'all':
+  def _build_source_dirs(self):
+    if self.category == self._default_category:
       for source in file(self._sources).read():
-        dirs.append(self._wallpaper_dir + '/' + source)
+        self.sources.append(os.path.join(self._wallpaper_dir,source))
 
-    else
+    else:
+      self.sources.append(os.path.join(self._wallpaper_dir,self.category))
 
-    return dirs
-
-  def _get_wallpapers(self):
-    wallpapers = []
-
+  def _build_wallpapers(self):
     for source in self.sources:
       for root, dirs, files in os.walk(source):
-        for file in files
-          wallpapers.append(os.path.join(root,file))
+        for file in files:
+          self.wallpapers.append(os.path.join(root,file))
 
-    return wallpapers
-
-  def parse_args(self):
+  def _parse_args(self):
     parser = argparse.ArgumentParser(description='Wallpaper changer thingus')
 
     parser.add_argument('-c',
@@ -136,7 +111,6 @@ class WallPaper(object):
         action='store_true')
 
     self.args = parser.parse_args()
-    self.__process_args()
 
   def lock(self):
     f = open(self._lock,'w')
@@ -163,5 +137,29 @@ class WallPaper(object):
 
 if __name__ == '__main__':
   wallpaper = WallPaper()
-  wallpaper.parse_args()
-  wallpaper.set_wallpaper()
+
+  if wallpaper.args.dump_cache:
+    wallpaper.dump_cache()
+
+  elif wallpaper.args.flush_cache:
+    wallpaper.flush_cache()
+
+  elif wallpaper.args.clear:
+    wallpaper.clear()
+
+  elif wallpaper.args.unlock:
+    wallpaper.unlock()
+
+  elif wallpaper.args.lock:
+    wallpaper.lock()
+
+  elif wallpaper.is_locked and not wallpaper.args.unlock:
+    print "currently locked, bailing out"
+
+  elif wallpaper.args.previous:
+    wallpaper.set_previous()
+
+  else:
+    wallpaper.set_wallpaper()
+
+
