9423dc8eb85eca742fec34f83081aa6cd5d00eed
Chris Scheller <schelcj@pobox.com>
2012-05-09 14:57:26 -0400

    just something to play around with

---
+++ disk_usge.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Number::Bytes::Human qw(format_bytes);
+use File::Slurp qw(read_dir);
+use Filesys::DiskUsage qw(du);
+use Data::Dumper;
+
+my @dirs  = map {qq{/home/$_}} read_dir('/home');
+my %sizes = du({'make-hash' => 1}, @dirs);
+
+say "Size\tDirectory";
+map {say format_bytes($sizes{$_}) . "\t" . $_} reverse sort {$sizes{$a} <=> $sizes{$b}} keys %sizes;
