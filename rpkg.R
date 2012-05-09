183698dd7d2d10b6cb6aa7edd49c016a103f3347
Chris Scheller <schelcj@pobox.com>
2012-05-09 14:35:11 -0400

    we just have to add the path to .libPaths() and any calls to install.packages() will attempt to install to the first path listed in .libPaths(). no need to explicity install to a lib= path now.

--- rpkg.R
+++ rpkg.R
@@ -7,12 +7,13 @@ base_repo <- '/home/software';
 codename  <- system("lsb_release -c|awk {'print $2'}", intern=TRUE);
 site_lib  <- paste(sep='/', base_repo, codename, 'R', getRversion(), 'site-library');
 
-opt <- getopt(matrix(c(
-'help',         'h', 0, "logical",   "Usage information",
-'package',      'p', 1, "character", "Package name to install",
-'bioconductor', 'b', 0, "logical",   "Package is a bioconductor package"
-),ncol=5,byrow=TRUE));
+.libPaths(site_lib);
 
+opt <- getopt(matrix(c(
+  'help',         'h', 0, "logical",   "Usage information",
+  'package',      'p', 1, "character", "Package name to install",
+  'bioconductor', 'b', 0, "logical",   "Package is a bioconductor package"
+),ncol=5,byrow=TRUE));
 
 if (!is.null(opt$help)) {
   writeLines("Usage: test.R [options]");
@@ -22,15 +23,9 @@ if (!is.null(opt$help)) {
   q(status=1);
 }
 
-if (!is.null(opt$bioconductor)) {
-  if (Sys.getenv('R_LIBS') == "") {
-    writeLines('R_LIBS env var is not set. Can not install bioconductor packages without it!');
-    writeLines(paste(sep="", 'set it with:\nexport R_LIBS=', site_lib));
-    q();
-  }
-
-  source("http://www.bioconductor.org/biocLite.R");
-  biocLite(opt$package, lib=site_lib);
+if (is.null(opt$bioconductor)) {
+  install.packages(opt$package, dependencies=TRUE, repos=mirror);
 } else {
-  install.packages(opt$package, dependencies=TRUE, lib=site_lib, repos=mirror);
+  source("http://www.bioconductor.org/biocLite.R");
+  biocLite(opt$package);
 }
