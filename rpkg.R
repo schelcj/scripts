e790f1c4493a60a6bb9b7341749346ea99fe9aca
Chris Scheller <schelcj@pobox.com>
2012-05-09 14:14:22 -0400

    trying to handle the bioconductor crap and failing

--- rpkg.R
+++ rpkg.R
@@ -2,13 +2,17 @@
 
 library(getopt);
 
+mirror    <- 'http://cran.mtu.edu/';
+base_repo <- '/home/software';
+codename  <- system("lsb_release -c|awk {'print $2'}", intern=TRUE);
+site_lib  <- paste(sep='/', base_repo, codename, 'R', getRversion(), 'site-library');
+
 opt <- getopt(matrix(c(
 'help',         'h', 0, "logical",   "Usage information",
 'package',      'p', 1, "character", "Package name to install",
 'bioconductor', 'b', 0, "logical",   "Package is a bioconductor package"
 ),ncol=5,byrow=TRUE));
 
-site_lib <- paste(sep='/', '/home/software/lucid/R', paste(sep='.', R.version$major, R.version$minor), 'site-library')
 
 if (!is.null(opt$help)) {
   writeLines("Usage: test.R [options]");
@@ -19,8 +23,14 @@ if (!is.null(opt$help)) {
 }
 
 if (!is.null(opt$bioconductor)) {
-  source("http://www.bioconductor.org/biocLite.R")
-  biocLite(opt$package, lib=site_lib)
+  if (Sys.getenv('R_LIBS') == "") {
+    writeLines('R_LIBS env var is not set. Can not install bioconductor packages without it!');
+    writeLines(paste(sep="", 'set it with:\nexport R_LIBS=', site_lib));
+    q();
+  }
+
+  source("http://www.bioconductor.org/biocLite.R");
+  biocLite(opt$package, lib=site_lib);
 } else {
-  install.packages(opt$package, dependencies=TRUE, lib=site_lib, repos='http://cran.mtu.edu/');
+  install.packages(opt$package, dependencies=TRUE, lib=site_lib, repos=mirror);
 }
