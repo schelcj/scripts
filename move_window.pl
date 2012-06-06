4549c0854f439d0c11043f8a64309fb4892d7325
Chris Scheller <schelcj@pobox.com>
2012-06-06 11:50:44 -0400

    handle a cancled selection and tidy up

--- move_window.pl
+++ move_window.pl
@@ -7,15 +7,17 @@ my $cmd_str   = q{zenity --list --text "Select Window" --column "Title"};
 my $wmctrl_fh = System::Command->new(q{wmctrl -l})->stdout();
 
 while (<$wmctrl_fh>) {
-  my ($id,$desktop,$host,@title) = split(/\s+/);
+  my ($id, $desktop, $host, @title) = split(/\s+/);
   my $title = join(' ', @title);
   $cmd_str .= qq[ "$title"];
 }
 $wmctrl_fh->close();
 
 my $zenity_fh = System::Command->new($cmd_str)->stdout();
-chomp(my $window = <$zenity_fh>);
+my $window    = <$zenity_fh>;
 $zenity_fh->close();
 
-System::Command->new(qq{wmctrl -r $window -e 0,0,0,-1,-1});
-System::Command->new(qq{wmctrl -R $window});
+if ($window) {
+  System::Command->new(qq{wmctrl -r $window -e 0,0,0,-1,-1});
+  System::Command->new(qq{wmctrl -R $window});
+}
