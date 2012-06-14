4291010a18befc17e8120c1a141ef067e0171fde
Chris Scheller <schelcj@pobox.com>
2012-06-14 11:38:52 -0400

    just a little playing with mojo dom parsing

---
+++ get_fac_list.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Mojo::UserAgent;
+use File::Slurp qw(read_file);
+use List::MoreUtils qw(any);
+
+my $url = q{http://www.sph.umich.edu/iscr/faculty/dept.cfm?deptID=1};
+my $css = q{tr td.list a strong};
+my @fac = read_file(qq[$ENV{HOME}/faculty.txt]);
+my $ua  = Mojo::UserAgent->new();
+my $dom = $ua->get($url)->res()->dom();
+
+$dom->find($css)->each(
+  sub {
+    my ($e, $i) = @_;
+    my ($name)  = split(/,/, $e->text);
+    next if any {lc($_) eq lc($name)} @fac;
+    say $name;
+  }
+);
