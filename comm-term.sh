d5fbe2453f445520202a8b7229f54565d93010b4
Chris Scheller <schelcj@pobox.com>
2012-08-01 11:00:20 -0400

    getting the port forwarding crap working right

--- comm-term.sh
+++ comm-term.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-gnome-terminal \
-  --profile 'comm' \
-  --command  "mosh -n --server='LANG=en_US.UTF-8 /storage/software/slackware12/mosh/1.2.2/bin/mosh-server' ressik.dyndns.org"
+#  --command  "mosh -n --server='LANG=en_US.UTF-8 /storage/software/slackware12/mosh/1.2.2/bin/mosh-server' ressik.dyndns.org"
+gnome-terminal --profile 'comm' --command "${HOME}/scripts/trapper.sh"
