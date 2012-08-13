e8b10a6098759d8ac2f36e8e4b08668577eaee88
Chris Scheller <schelcj@pobox.com>
2012-08-12 20:39:04 -0400

    just a quicky to control the fan on this old workstation

---
+++ fan.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dev="/sys/class/hwmon/hwmon0/device/pwm2"
+
+case "$1" in
+  'max')
+    echo 240 > $dev
+    ;;
+  'med')
+    echo 128 > $dev
+    ;;
+  'low')
+    echo 0 > $dev
+    ;;
+  'status')
+    cat $dev
+    ;;
+  *)
+    echo "Usage: $0 [max|med|low|status]"
+    exit 1
+esac
+
+exit 0
