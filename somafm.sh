9ffade9811fbf90a00bed7d3852d0122f6838813
Chris Scheller <schelcj@pobox.com>
2012-08-23 14:34:14 -0400

    was missing some stations and some other minor changes

--- somafm.sh
+++ somafm.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
+tempfile=$(tempfile 2>/dev/null) || tempfile=/tmp/test$$
 trap "rm -f $tempfile" 0 1 2 5 15
 
 while true; do
   dialog \
     --backtitle "SomaFM: Listener Supported, Commercial Free Internet Radio" \
-    --title "Select a station to listen to" --clear \
-    --radiolist "Select a station to listen to" 20 40 18 \
-      "doomed"               "Doomed"                 off \
+    --title "Select a station to listen to" \
+    --clear \
+    --radiolist "Select a station to listen to" 20 60 24 \
       "groovesalad"          "Groove Salad"           off \
       "lush"                 "Lush"                   off \
-      "suburbsofgoa"         "Suburbs of Goa"         off \
-      "secretagent"          "Secret Agent"           off \
       "dronezone"            "Drone Zone"             off \
-      "spacestation"         "Space Station"          off \
-      "cliqhopidm"           "cliqhop idm"            off \
-      "digitalis"            "Digitalis"              off \
       "sonicuniverse"        "Sonic Universe"         off \
-      "bootliquor"           "Boot Liquor"            off \
-      "covers"               "Covers"                 off \
-      "illinoisstreetlounge" "Illinois Street Lounge" off \
+      "digitalis"            "Digitalis"              off \
       "indiepoprocks"        "indie pop rocks"        off \
       "poptron"              "PopTron"                off \
-      "tagstrip"             "Tags Trip"              off \
+      "480min"               "480 Minutes"            off \
+      "covers"               "Covers"                 off \
+      "u80s"                 "Underground 80s"        off \
+      "secretagent"          "Secret Agent"           off \
+      "suburbsofgoa"         "Suburbs of Goa"         off \
       "beatbender"           "Beat Bender"            off \
-      "missioncontrol"       "Mission Control"        off 2> $tempfile 
+      "dubstep"              "Dub Step Beyond"        off \
+      "cliqhopidm"           "cliqhop idm"            off \
+      "spacestation"         "Space Station"          off \
+      "missioncontrol"       "Mission Control"        off \
+      "bootliquor"           "Boot Liquor"            off \
+      "illinoisstreetlounge" "Illinois Street Lounge" off \
+      "tagstrip"             "Tags Trip"              off \
+      "doomed"               "Doomed"                 off \
+      "brfm"                 "Black Rock FM"          off \
+      "sxfm"                 "South by Soma"          off \
+      "sf1033"               "SF 10-33"               off 2> $tempfile 
 
-  rv=$?
-  if [ $rv == 1 ]; then
-    exit
-  fi
-
+  test $? == 1 && exit
   station="$(cat $tempfile)"
   mplayer -vo none -ao sdl http://somafm.com/startstream=${station}.pls
 done
