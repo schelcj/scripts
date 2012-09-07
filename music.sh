b7f561b03e0b2a98d2ea67424ec6af83261da6ec
Chris Scheller <schelcj@pobox.com>
2012-09-07 09:32:47 -0400

    just a setup for my music players

--- music.sh
+++ music.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+tmux start-server
+tmux has-session -t music 2>&1 > /dev/null
+
+if [ $? -ne 0 ]; then
+  tmux new-session -d -s music -n somafm 'somafm.sh' 
+  tmux new-window -a -d -n pandora 'pianobar'
+  tmux new-window -a -d -n mp3 'mp3blaster'
+fi
+
 tmux attach -t music
