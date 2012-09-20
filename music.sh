75f8e7d429e90a68e7a935702111d5fc79463c72
Chris Scheller <schelcj@pobox.com>
2012-09-20 15:47:29 -0400

    just random playlists i can stream from my home setup. why didn't i realize i could use mplayer for this a long time ago?

--- music.sh
+++ music.sh
@@ -7,6 +7,7 @@ if [ $? -ne 0 ]; then
   tmux new-session -d -s music -n somafm 'somafm.sh' 
   tmux new-window -a -d -n pandora 'pianobar'
   tmux new-window -a -d -n mp3 'mp3blaster'
+  tmux new-window -a -d -n mplayer 'cd ~/Dropbox/Media/Music/Playlists'
 fi
 
 tmux attach -t music
