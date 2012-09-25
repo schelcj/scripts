6b8eab358c69643fccf87ab472558a0b479086b3
Chris Scheller <schelcj@pobox.com>
2012-09-25 16:09:21 -0400

    changing my setup

--- dock.sh
+++ dock.sh
@@ -2,7 +2,8 @@
 
 export PATH=${HOME}/scripts:${HOME}/bin:${PATH}
 
-xrandr -d :0.0 --output VGA1 --auto --primary --mode 1920x1200 --output LVDS1 --off
+#xrandr -d :0.0 --output LVDS1 --auto --output VGA1 --mode 1920x1200 --primary --right-of LVDS1
+#xrandr -d :0.0 --output VGA1 --auto --primary --mode 1920x1200 --output LVDS1 --off
 nmcli nm wifi off
 nmcli con up id 'Biostat Cluster'
 wallpaper.pl
