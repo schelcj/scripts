5ac9a5e805917370cbf8eec4d7d702a1cbf5d58f
Chris Scheller <schelcj@pobox.com>
2012-10-30 08:37:31 -0400

    window for streaming from my home box

--- music.sh
+++ music.sh
@@ -6,8 +6,7 @@ tmux has-session -t music 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
   tmux new-session -d -s music -n somafm 'somafm.sh' 
   tmux new-window -a -d -n pandora 'pianobar'
-  tmux new-window -a -d -n mp3 'mp3blaster'
-  tmux new-window -a -d -n mplayer 'cd ~/Dropbox/Media/Music/Playlists'
+  tmux new-window -a -d -n ressik 
 fi
 
 tmux attach -t music
