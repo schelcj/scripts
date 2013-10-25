600051b7bee954005e45f608a706a648f8acc61e
Chris Scheller <schelcj@pobox.com>
2013-10-25 10:36:28 -0400

    new script to pull all the stations from the site so it is always up to date with new stations

---
+++ somafm.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Mojo::UserAgent;
+use Mojo::DOM;
+use IPC::System::Simple qw(run);
+use File::Temp;
+use File::Slurp qw(read_file);
+use Try::Tiny;
+
+my $backtitle          = q{SomaFM: Listener Supported, Commercial Free Internet Radio};
+my $title              = q{Select a station to listen to};
+my $radiolist_title    = q{Select a station to listen to};
+my $dialog_geom        = q{20 60};
+my $dialog_list_height = q{24};
+my $somafm_url         = q{http://somafm.com};
+my $somafm_stream_url  = qq{$somafm_url/startstream=%s.pls};
+my $mplayer_cmd        = qq{mplayer -quiet -vo none -ao sdl %s 2>&1 | dialog --progressbox $dialog_geom};
+
+while (1) {
+  play(get_station_selection());
+}
+
+sub play {
+  my ($station)   = @_;
+  my $url         = sprintf $somafm_stream_url, $station;
+  my $mplayer_cmd = sprintf $mplayer_cmd, $url;
+
+  run($mplayer_cmd);
+}
+
+sub get_station_selection {
+  my $temp   = File::Temp->new();
+  my $dialog = build_station_selection_dialog($temp->filename);
+
+  try {
+    run($dialog);
+  } catch {
+    exit;
+  };
+
+  return read_file($temp->filename);
+}
+
+sub build_station_selection_dialog {
+  my ($temp_file) = @_;
+
+  my $cmd    = qq{dialog --backtitle "$backtitle" --title "$title" --clear --radiolist "$radiolist_title" $dialog_geom $dialog_list_height };
+  map {$cmd .= qq{"$_->[1]" "$_->[0]" off }} get_stations();
+  $cmd      .= qq{2> $temp_file};
+  $cmd      =~ s/\!/\\!/g;
+
+  return $cmd;
+}
+
+sub get_stations {
+  my $agent    = Mojo::UserAgent->new();
+  my $dom      = Mojo::DOM->new($agent->get($somafm_url)->res->body);
+  my @stations = ();
+
+  $dom->find('li.cbshort')->each(
+    sub {
+      my $node = shift;
+      (my $name = $node->at('a:first-child')->attrs('href')) =~ s/\///g;
+      my $title = $node->at('h3')->text();
+
+      push @stations, [$title, $name];
+    }
+  );
+
+  return sort {lc($a->[0]) cmp lc($b->[0])} @stations;
+}
