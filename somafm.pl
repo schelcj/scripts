410097ec72b2fc1e548766a2b6a7cc61f0aa67ac
Chris Scheller <schelcj@pobox.com>
2013-10-25 13:36:34 -0400

    mostly cosmetic shit

--- somafm.pl
+++ somafm.pl
@@ -8,25 +8,25 @@ use File::Temp;
 use File::Slurp qw(read_file);
 use Try::Tiny;
 
+my $somafm_url         = q{http://somafm.com};
 my $backtitle          = q{SomaFM: Listener Supported, Commercial Free Internet Radio};
 my $title              = q{Select a station to listen to};
 my $radiolist_title    = q{Select a station to listen to};
 my $dialog_geom        = q{20 60};
 my $dialog_list_height = q{24};
-my $somafm_url         = q{http://somafm.com};
-my $somafm_stream_url  = qq{$somafm_url/startstream=%s.pls};
-my $mplayer_cmd_fmt    = qq{mplayer -quiet -vo none -ao sdl %s 2>&1 | dialog --progressbox $dialog_geom};
 
 while (1) {
   play(get_station_selection());
 }
 
 sub play {
-  my ($station)   = @_;
-  my $url         = sprintf $somafm_stream_url, $station;
-  my $mplayer_cmd = sprintf $mplayer_cmd_fmt, $url;
+  my ($station) = @_;
 
-  run($mplayer_cmd);
+  my $url         = sprintf qq{$somafm_url/startstream=%s.pls}, $station;
+  my $mplayer_cmd = sprintf q{mplayer -quiet -vo none -ao sdl %s 2>&1}, $url;
+
+  run(qq{$mplayer_cmd | dialog --progressbox $dialog_geom});
+
   return;
 }
 
@@ -46,12 +46,13 @@ sub get_station_selection {
 sub build_station_selection_dialog {
   my ($temp_file) = @_;
 
-  my $cmd    = qq{dialog --backtitle "$backtitle" --title "$title" --clear --radiolist "$radiolist_title" $dialog_geom $dialog_list_height };
-  map {$cmd .= qq{"$_->[1]" "$_->[0]" off }} get_stations();
-  $cmd      .= qq{2> $temp_file};
-  $cmd      =~ s/\!/\\!/g;
+  my $cmd  = qq{dialog --backtitle "$backtitle" };
+     $cmd .= qq{--title "$title" --clear --radiolist };
+     $cmd .= qq{"$radiolist_title" $dialog_geom $dialog_list_height };
 
-  return $cmd;
+  map {$cmd .= qq{"$_->[1]" "$_->[0]" off }} get_stations();
+
+  return qq{$cmd 2> $temp_file};
 }
 
 sub get_stations {
