ce165bf5bab8ae231452c063df722f1b61d35040
Chris Scheller <schelcj@pobox.com>
2014-01-16 15:06:26 -0500

    reworked to use a playlist file to pick up the next station when tracks end

--- di.pl
+++ di.pl
@@ -5,19 +5,20 @@ use Mojo::UserAgent;
 use Mojo::DOM;
 use IPC::System::Simple qw(run);
 use File::Temp;
-use File::Slurp qw(read_file);
+use File::Slurp qw(read_file write_file append_file);
 use Try::Tiny;
 use URI;
 
 my $di_url             = q{http://www.di.fm/};
-my $di_fm_url          = q{http://pub1.di.fm:80};
+my $di_fm_url          = q{http://pub%d.di.fm:80/di_%s};
 my $backtitle          = q{Digitally Imported: Addictive Electronic Music};
 my $title              = q{Select a channel to listen to};
 my $radiolist_title    = q{Select a channel to listen to};
 my $dialog_geom        = q{20 60};
 my $dialog_list_height = q{24};
+my $di_fm_pub_range    = 6;
 my @channels           = get_channels($di_url);
-
+my $mplayer_opts       = q{-prefer-ipv4 -nolirc -quiet -vo none -ao sdl -input file=/tmp/mplayer};
 
 while (1) {
   play(get_station_selection());
@@ -26,8 +27,10 @@ while (1) {
 sub play {
   my ($station) = @_;
 
-  my $url = sprintf qq{$di_fm_url/di_%s}, $station;
-  my $mplayer_cmd = sprintf q{mplayer -quiet -vo none -ao sdl -input file=/tmp/mplayer %s 2>&1}, $url;
+  my $temp = File::Temp->new(SUFFIX => '.pls', UNLINK => 0);
+  build_playlist($station, $di_fm_pub_range, $temp->filename);
+
+  my $mplayer_cmd = qq{mplayer $mplayer_opts -playlist } . $temp->filename . q{ 2>&1};
 
   run(qq{$mplayer_cmd | dialog --progressbox $dialog_geom});
 
@@ -60,6 +63,21 @@ sub build_station_selection_dialog {
   return qq{$cmd 2> $temp_file};
 }
 
+sub build_playlist {
+  my ($station, $range, $file) = @_;
+
+  write_file($file, qq{[playlist]\n});
+  append_file($file, qq{NumberOfEntries=$range\n});
+
+  for (1..$range) {
+    my $url = sprintf $di_fm_url, $_, $station;
+
+    append_file($file, qq{File$_=$url\n});
+    append_file($file, qq{Title$_=$station\n});
+    append_file($file, qq{Length$_=-1\n});
+  }
+}
+
 sub get_channels {
   my ($url)    = @_;
   my $agent    = Mojo::UserAgent->new();
@@ -68,8 +86,8 @@ sub get_channels {
 
   $dom->find('div.lists ul li')->each(
     sub {
-      my $node          = shift;
-      (my $channel_url   = URI->new($node->at('span.play')->attr('data-tunein-url'))->path()) =~ s/\///g;
+      my $node = shift;
+      (my $channel_url = URI->new($node->at('span.play')->attr('data-tunein-url'))->path()) =~ s/\///g;
       my $channel_title = $node->at('a span')->text();
 
       push @channels, [$channel_title, $channel_url];
