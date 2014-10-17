2458e1ea6a11b714264aa4eaa4317642e04e63de
Chris Scheller <schelcj@pobox.com>
2014-10-17 13:40:32 -0400

    not doing two way sync on this one. it is just for reference in EN

--- sync_wikis.sh
+++ sync_wikis.sh
@@ -2,7 +2,7 @@
 
 export PATH=/usr/local/bin:$PATH
 
-gnsync -p ~/Dropbox/Wikis/Default -f markdown -n Wiki -l ~/tmp/gnsync.log -t TWO_WAY
+gnsync -p ~/Dropbox/Wikis/Default -f markdown -n Wiki -l ~/tmp/gnsync.log
 
 # XXX - causes duplicate notes to be created, why?
 #gnsync -p ~/Dropbox/Wikis/Default/journal -f markdown -n Journal -l ~/tmp/gnsync.log -t TWO_WAY
