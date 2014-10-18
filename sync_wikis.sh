b07f9611df2bd1bbee18c20062e9794096d5d608
Chris Scheller <schelcj@pobox.com>
2014-10-18 12:31:33 -0400

    forgot to commit this yesterday

--- sync_wikis.sh
+++ sync_wikis.sh
@@ -3,6 +3,7 @@
 export PATH=/usr/local/bin:$PATH
 
 gnsync -p ~/Dropbox/Wikis/Default -f markdown -n Wiki -l ~/tmp/gnsync.log
+gnsync -p ~/Dropbox/Wikis/Labbooks -f markdown -n Labbooks -l ~/tmp/gnsync.log
 
 # XXX - causes duplicate notes to be created, why?
 #gnsync -p ~/Dropbox/Wikis/Default/journal -f markdown -n Journal -l ~/tmp/gnsync.log -t TWO_WAY
