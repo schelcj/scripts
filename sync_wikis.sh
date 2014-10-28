5f957423501e612c39ba84205fde94a38178b3c6
Chris Scheller <schelcj@pobox.com>
2014-10-28 09:27:53 -0400

    just another vimwiki to sync to EN

--- sync_wikis.sh
+++ sync_wikis.sh
@@ -4,6 +4,7 @@ export PATH=/usr/local/bin:$PATH
 
 gnsync -p ~/Dropbox/Wikis/Default -f markdown -n Wiki -l ~/tmp/gnsync.log
 gnsync -p ~/Dropbox/Wikis/Labbooks -f markdown -n Labbooks -l ~/tmp/gnsync.log
+gnsync -p ~/Dropbox/Wikis/ConcertPharma -f markdown -n ConcertPharma -l ~/tmp/gnsync.log
 
 # XXX - causes duplicate notes to be created, why?
 #gnsync -p ~/Dropbox/Wikis/Default/journal -f markdown -n Journal -l ~/tmp/gnsync.log -t TWO_WAY
