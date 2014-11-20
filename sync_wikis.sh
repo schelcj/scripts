288ab40a201d8c9287eafa182130493cf08ca4fb
Chris Scheller <schelcj@pobox.com>
2014-11-20 09:51:56 -0500

    changed notebook names and forgot. also take advantage of bash string manip stuffs

--- sync_wikis.sh
+++ sync_wikis.sh
@@ -8,14 +8,14 @@ WIKIS=(
   Default:Wiki
   Labbooks:Labbooks
   ConcertPharma:ConcertPharma
-  SPH-ICS:SPH-ICS
-  Biostat:Biostat
-  Flux:Flux
+  SPH-ICS:Umich-SPH-ICS
+  Biostat:Umich-Biostat
+  Flux:Umich-Flux
 )
 
 for line in "${WIKIS[@]}"; do
-  wiki=$(echo $line|cut -d\: -f1)
-  notebook=$(echo $line|cut -d\: -f2)
+  wiki=${line%:*}
+  notebook=${line#*:}
 
   gnsync -p $WIKI_DIR/$wiki -f markdown -n "$notebook" -l $LOG
 done
