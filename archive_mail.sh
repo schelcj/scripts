d7ee7a2322089308a40efa7599fbdccc1a531c1a
Chris Scheller <schelcj@pobox.com>
2014-12-15 11:28:34 -0500

    indexing the whole archive was getting into oom problems

--- archive_mail.sh
+++ archive_mail.sh
@@ -3,6 +3,5 @@
 for acct in pobox umich; do
   getmail -v --rcfile getmailrc-${acct}
   mu index --nocleanup --maildir="~/Mail/${acct^}/[Gmail].Sent\ Mail" --muhome=~/.mu-sent-index
+  mu index --maildir ~/Mail/${acct^}/$(date '+%Y')/$(date '+%m')/
 done
-
-/usr/bin/mu index --maildir ${HOME}/Mail
