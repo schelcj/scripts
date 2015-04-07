7d54704271e501886762441612278371b933edc8
Chris Scheller <schelcj@pobox.com>
2015-04-07 09:48:57 -0400

    try syncing these wikis to different notebooks to see if that fixes the duplication problem

--- sync_wikis.sh
+++ sync_wikis.sh
@@ -5,19 +5,21 @@ export PATH=/usr/local/bin:$PATH
 LOG=~/tmp/gnsync.log
 WIKI_DIR=$HOME/Dropbox/Wikis
 WIKIS=(
-  Default:Wiki
-  Labbooks:Labbooks
-  CSG:CSG
-  ConcertPharma:ConcertPharma
-  Flux:Flux
-  SPH:SPH
-  Biostat:Biostat
-  MyNorth:MyNorth
+  Default:Default-Wiki
+  Labbooks:Labbooks-Wiki
+  CSG:CSG-Wiki
+  ConcertPharma:ConcertPharma-Wiki
+  Flux:Flux-Wiki
+  SPH:SPH-Wiki
+  Biostat:Biostat-Wiki
+  MyNorth:MyNorth-Wiki
 )
 
 for line in "${WIKIS[@]}"; do
   wiki=${line%:*}
   notebook=${line#*:}
 
+  echo -n "Syncing $wiki to $notebook - "
   gnsync -p $WIKI_DIR/$wiki -f markdown -n "$notebook" -l $LOG
+  echo "Done"
 done
