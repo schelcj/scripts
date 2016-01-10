75ae5f93a87f3dc0c60bb0e49f17bb05b2b5c167
Chris Scheller <schelcj@pobox.com>
2016-01-10 15:18:19 -0500

    going to try this out for a bit

--- rofi.sh
+++ rofi.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+#  -location 1 \
+#  -width 100 \
+#  -switchers "window,run,ssh,Workspaces:i3_switch_workspaces.sh" \
+#  -bg "#484e50" \
+#  -fg "#ffffff" \
+
 rofi \
-  -location 1 \
-  -width 100 \
-  -bg "#484e50" \
-  -fg "#ffffff" \
   -padding 0 \
   -ssh \
   -set \
   -title true \
-  -switchers "window,run,ssh,Workspaces:i3_switch_workspaces.sh" \
-  -show window
+  -switchers "run,window,ssh" \
+  -show run
