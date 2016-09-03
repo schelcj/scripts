89b32fdbef0f4fa2999cdf4bb72b2fc378048bcc
Chris Scheller <schelcj@pobox.com>
2016-09-02 20:53:31 -0400

    change up rofi

--- rofi.sh
+++ rofi.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #  -bg "#484e50" \
 #  -fg "#ffffff" \
+#  -width 100 \
+#  -location 1 \
+#  -lines 5 \
 
 rofi \
-  -location 1 \
-  -width 100 \
-  -padding 0 \
-  -lines 5 \
+  -padding 1 \
   -ssh \
   -set \
   -title true \
   -switchers "window,run,ssh,Workspaces:i3_switch_workspaces.sh" \
-  -show run
+  -show window
