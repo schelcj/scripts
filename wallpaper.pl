16809f62fd0a32f6834eda6a01eeeeca3834dcb2
Chris Scheller <schelcj@merit.edu>
2017-12-22 19:47:17 -0500

    reworking quite a bit to simplify

--- wallpaper.pl
+++ wallpaper.pl
@@ -2,125 +2,55 @@
 
 use local::lib;
 use Modern::Perl;
-use Carp qw(confess);
 use DB_File;
-use File::Slurp qw(read_file write_file append_file);
-use Getopt::Compact;
 use Data::Dumper;
 use File::Find::Object;
-use File::Flock::Tiny;
-use Proc::Daemon;
-use IPC::System::Simple qw(capture);
+use Getopt::Long qw(HelpMessage);
+use File::Spec;
+use IO::All;
 
 my $PREFIX           = qq($ENV{HOME}/.wallpapers);
-my $TMPDIR           = qq($ENV{HOME}/tmp);
 my $LOCK             = qq{$PREFIX/lock};
-my $HISTORY          = qq{$PREFIX/history};
 my $CATEGORY         = qq{$PREFIX/category};
 my $WALLPAPER_DIR    = qq{$PREFIX/Wallpapers};
 my $CURRENT          = qq{$PREFIX/current};
 my $PREVIOUS         = qq{$PREFIX/previous};
-my $LOG              = qq{$PREFIX/log};
 my $SOURCES          = qq{$PREFIX/sources};
-my $PIDFILE          = qq{$PREFIX/wallpaper.pid};
-my $SLEEP_INTERVAL   = 60*15;
-my $BGSETTER         = q{fbsetbg};
-my $BGSETTER_OPTS    = q{-f};
-my $SLASH            = q{/};
 my $DEFAULT_CATEGORY = q{all};
 
-## no tidy
-my $opts = Getopt::Compact->new(
-  struct => [
-    [[qw(c category)],    q(Wallpaper category),                                ':s'],
-    [[qw(f flush-cache)], q(Flush the wallpaper cache)                              ],
-    [[qw(d dump-cache)],  q(Dump the wallpaper cache)                               ],
-    [[qw(l lock)],        q(Lock the current paper)                                 ],
-    [[qw(u unlock)],      q(Unlock the current paper)                               ],
-    [[qw(clear)],         q(Clear previous category)                                ],
-    [[qw(p previous)],    q(Set wallpaper to previous paper)                        ],
-    [[qw(s sleep)],       q(How long to sleep, in seconds, if run as a daemon), ':i'],
-    [[qw(D daemon)],      q(Background process for a slide show effect)             ],
-    [[qw(stop)],          q(Stop a running daemon)                                  ],
-    [[qw(i image)],       q(Set image as wallpaper),                            ':s'],
-  ]
-)->opts();
-## end no tidy
-
-exit if -e $LOCK and not $opts->{unlock};
-
 my %history = ();
-tie %history, 'DB_File', $HISTORY; ## no critic (ProhibitTies)
+tie %history, 'DB_File', qq{$PREFIX/history};    ## no critic (ProhibitTies)
 
-if ($opts->{clear}) {
-  unlink $CATEGORY;
-}
+GetOptions(
+  'category|c=s'  => sub {$_[1] > io($CATEGORY)},
+  'clear'         => sub {unlink $CATEGORY},
+  'flush-cache|f' => sub {flush_cache()},
+  'lock|l'        => sub {2 > io($LOCK)},
+  'unlock|u'      => sub {unlink($LOCK)},
+  'previous|p'    => sub {
+    set_wallpaper(io($PREVIOUS)->all);
+    exit;
+  },
+  'image|i=s' => sub {
+    set_wallpaper($_[1]);
+    exit;
+  },
+  'dump-cache|d' => sub {
+    print {*STDOUT} Dumper \%history;
+    exit;
+  },
+  'h|help' => \&HelpMessage,
+  )
+  or HelpMessage(1);
 
-if ($opts->{category}) {
-  write_file($CATEGORY, $opts->{category});
-}
+exit if -e $LOCK;
 
-if ($opts->{'flush-cache'}) {
-  flush_cache();
-}
-
-if ($opts->{'dump-cache'}) {
-  print {*STDOUT} Dumper \%history;
-  exit;
-}
-
-if ($opts->{unlock}) {
-  unlink $LOCK;
-}
-
-if ($opts->{lock}) {
-  write_file($LOCK, '2');
-}
-
-if ($opts->{previous}) {
-  my $paper = read_file($PREVIOUS);
-  set_wallpaper($paper);
-  exit;
-}
-
-if ($opts->{stop}) {
-  my $pid = read_file($PIDFILE);
-  kill 9, $pid;
-  unlink $PIDFILE;
-  exit;
-}
-
-if ($opts->{image}) {
-  set_wallpaper($opts->{image});
-  exit;
-}
-
-# XXX this isn't working as i want
-# XXX i'm getting the wrong pid
-my $lock = File::Flock::Tiny->write_pid($PIDFILE) or die q{Another wallpaper.pl process is running};
-
-if ($opts->{daemon}) {
-    my $daemon = Proc::Daemon->new(work_dir => $TMPDIR);
-    my $pid    = $daemon->Init;
-
-    if (not $pid) {
-      while (1) {
-        set() if not -e $LOCK;
-        sleep($opts->{sleep}||$SLEEP_INTERVAL);
-      }
-    }
-
-    $daemon->Kill_Daemon();
-} else {
-  set();
-}
-
-$lock->release();
+set() unless -e $LOCK;
 
 sub set {
-    my @wallpaper_dirs = get_wallpaper_dirs();
-    my @wallpapers     = get_wallpapers(@wallpaper_dirs);
-    return _set(\@wallpapers);
+  my @wallpaper_dirs = get_wallpaper_dirs();
+  my @wallpapers     = get_wallpapers(@wallpaper_dirs);
+  return _set(\@wallpapers);
 }
 
 sub _set {
@@ -129,12 +59,12 @@ sub _set {
   my $set_paper = 0;
 
   if (scalar @{$papers} == 1) {
-    $rc = set_wallpaper($papers->[0]);
+    $rc        = set_wallpaper($papers->[0]);
     $set_paper = 1;
   } else {
     while (my $paper = get_random_wallpaper($papers)) {
-      next if is_cached($paper);
-      $rc = set_wallpaper($paper);
+      next if exists $history{shift};
+      $rc        = set_wallpaper($paper);
       $set_paper = 1;
       last;
     }
@@ -149,33 +79,28 @@ sub _set {
   return _set($papers);
 }
 
-sub _build_path {
-  my ($dir) = @_;
-  chomp $dir;
-  return qq{$WALLPAPER_DIR/$dir};
-}
-
 sub get_wallpaper_dirs {
   my @paths = ();
-  my $category = get_category();
+  my $category = (-e $CATEGORY) ? io($CATEGORY)->getline : $DEFAULT_CATEGORY;
 
   if ($category eq $DEFAULT_CATEGORY) {
-    return map {_build_path($_)} read_file($SOURCES);
+
+    return map {File::Spec->join($WALLPAPER_DIR, $_)} io($SOURCES)->chomp->getlines;
   } else {
     push @paths, $WALLPAPER_DIR;
   }
 
   push @paths, $category;
 
-  my $dir = join($SLASH, @paths);
-  confess qq{Wallpaper directory ($dir) does not exist} if not -e $dir;
+  my $dir = File::Spec->join(@paths);
+  die qq{Wallpaper directory ($dir) does not exist} if not -e $dir;
 
   return ($dir);
 }
 
 sub get_wallpapers {
   my (@dirs) = @_;
-  my $tree   = File::Find::Object->new({}, @dirs);
+  my $tree = File::Find::Object->new({}, @dirs);
   my @papers = ();
 
   while (my $leaf = $tree->next()) {
@@ -198,42 +123,50 @@ sub get_random_wallpaper {
 sub set_wallpaper {
   my ($paper) = @_;
 
-  my $cmd_str = sprintf q{%s %s}, get_bgsetter(), $paper;
-  my $rv      = capture($cmd_str);
+  my $rv = io->pipe(qq{fbsetbg -f $paper})->all;
 
-  cache($paper);
-  set_current($paper);
+  $history{$paper} = 1;
+  rename $CURRENT, $PREVIOUS;
+  shift > io($CURRENT);
 
   return $rv;
 }
 
-sub set_current {
-  my ($paper) = @_;
-  rename $CURRENT, $PREVIOUS;
-  write_file($CURRENT, $paper);
-  return;
-}
-
-sub get_category {
-  return (-e $CATEGORY) ? read_file($CATEGORY) : $DEFAULT_CATEGORY;
-}
-
-sub get_bgsetter {
-  return sprintf q{%s %s}, $BGSETTER, $BGSETTER_OPTS;
-}
-
-sub cache {
-  my ($paper) = @_;
-  $history{$paper} = 1;
-  return;
-}
-
-sub is_cached {
-  my ($paper) = @_;
-  return exists $history{$paper};
-}
-
 sub flush_cache {
   %history = ();
-  return;
 }
+
+__END__
+
+=head1 NAME
+
+wallpaper.pl - Script to manage my desktop wallpapers.
+
+=head1 SYNOPSIS
+
+wallpaper.pl [OPTIONS]
+
+  Options:
+
+    -c, --category    Wallpaper category
+    -f, --flush-cache Flush the wallpaper cache
+    -d, --dump-cache  Dump the current wallpaper cache to STDOUT
+    -l, --lock        Lock the current wallpaper
+    -u, --unlock      Unlock the current wallpaper
+        --clear       Clear the previous wallpaper category
+    -p, --previous    Set the wallpaper to the previous paper
+    -i, --image       Set image as the current wallpaper
+
+=head1 DESCRIPTION
+
+This sciprt is used to manage the desktop wallpaper displayed on my system.
+
+=head1 ENVIRONMENT VARIABLES
+
+None
+
+=head1 VERSION
+
+0.2
+
+=cut
