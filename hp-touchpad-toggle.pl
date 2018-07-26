e172e131750d0acc10c4e10e20752f7ebaa30c9c
Chris Scheller <schelcj@merit.edu>
2018-07-26 14:16:55 -0400

    quick script to enable/disable this damn touchpad

---
+++ hp-touchpad-toggle.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use IO::All;
+
+my $file = "$ENV{HOME}/.hp-touchpad-state";
+'enable' > io($file) unless -e $file;
+
+for (io->pipe('xinput')->chomp->getlines()) {
+  next unless /ALP000/;
+
+  if (/id=([^\s]+)/) {
+    my $state  = io($file)->all;
+    my $toggle = ($state eq 'disable') ? 'enable' : 'disable';
+
+    system("xinput $toggle $1");
+
+    $toggle > io($file);
+  }
+}
