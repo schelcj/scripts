163c890e1fd3fcb753114bd4fa862cf0212efee4
Chris Scheller <schelcj@pobox.com>
2018-08-18 14:30:00 -0400

    more changes

--- file_pics.pl
+++ file_pics.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+
 use POSIX qw(strftime);
 use English qw(-no_match_vars);
 use Carp qw(croak);
@@ -12,24 +13,28 @@ use File::Stat;
 use IO::All;
 use YASF;
 
-my $BASE     = '/home/schelcj/Media/Home Movies';
+my $BASE     = '/home/schelcj/Media/Pictures';
 my $dest_dir = YASF->new('{base}/{year}/{month}');
 
-for my $file (io->dir("$BASE/unfiled")->all_files) {
+for my $file (io->dir("$BASE/2018/08")->all_files) {
   my $stat  = File::Stat->new($file->name);
 
   my $dir = $dest_dir % {
     base  => $BASE,
-    year  => strftime('%Y',localtime $stat->ctime),
-    month => strftime('%m',localtime $stat->ctime),
+    year  => strftime('%Y',localtime $stat->mtime),
+    month => strftime('%m',localtime $stat->mtime),
   };
 
   make_path($dir) unless -e $dir;
   my $final = File::Spec->join($dir, basename($file));
 
+  next if $file eq $final;
+
   print "Moving $file to $final - ";
 
-  rename $file, $final or croak $ERRNO;
-
-  print "Done\n";
+  unless (rename($file, $final)) {
+    print "Failed\n";
+  } else {
+    print "Done\n";
+  }
 }
