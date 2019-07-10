661c75b0f8629d9c37a00d2e3d8671e65eb78e3a
Chris Scheller <schelcj@pobox.com>
2019-07-10 06:50:46 -0400

    changes due to ubuntu18/kernel/X bug that kills my screens

--- dock.sh
+++ dock.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-xrandr --output DP-2-2 --primary
-sleep 2
-xrandr --output eDP-1 --off
-sleep 2
+#xrandr --output DP-2-1 --auto
+#sleep 2
+#xrandr --output DP-2-1 --primary
+#sleep 2
+#xrandr --output eDP-1 --off
+#sleep 2
+
+xrandr --output DP-2-1 --off
+xrandr --output DP-2-1 --auto
+xrandr --output DP-2-1 --right-of eDP-1
+xrandr --output DP-2-1 --primary
+
 i3-msg restart
