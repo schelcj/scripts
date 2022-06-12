07166e64f8c744a8eb37d663c7263df909b42d52
Chris Scheller <schelcj@pobox.com>
2022-06-12 11:33:53 -0400

    cleaner way to get the zipcode

--- weather.sh
+++ weather.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-ZIP="$(curl -s ipinfo.io|grep postal|awk {'print $2'}|sed 's/"//g')"
-notify-send -u low "$(weather $ZIP)"
+# ZIP="$(curl -s ipinfo.io|grep postal|awk {'print $2'}|sed 's/"//g')"
+# ZIP=$(cat .zipcode)
+ZIP=$(curl -s ipinfo.io|jq -r '.postal')
+notify-send -u low "$(weather -f $ZIP)"
