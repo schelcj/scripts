517b2048c325383a7fa2f1228fad14591b5d9e48
Chris Scheller <schelcj@pobox.com>
2024-03-04 19:21:02 -0500

    no idea

--- rofi.sh
+++ rofi.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
-#  -bg "#484e50" \
-#  -fg "#ffffff" \
-#  -width 100 \
-#  -location 1 \
-#  -lines 5 \
+export PATH=$HOME/scripts:$PATH
 
 SHOW=$1
 
 rofi \
-  -padding 1 \
   -ssh \
-  -set \
   -title true \
   -switchers "window,run,drun,ssh,Workspaces:i3_switch_workspaces.sh" \
-  -show ${SHOW:='run'} \
-  -width 35 \
-  -location 0 \
-  -lines 8
+  -show ${SHOW:='run'}
+#  -location 1 \
+#  -theme-str 'window {width: 100%;}' \
 
