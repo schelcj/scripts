1d03a10f320e085a7e7616d018973f6336c1035b
Chris Scheller <schelcj@pobox.com>
2025-03-01 15:51:33 -0500

    fixup on pathing

--- rofi.sh
+++ rofi.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-export PATH=$HOME/scripts:$PATH
-
 export PATH=$HOME/scripts:$HOME/bin:$PATH
 SHOW=$1
 
@@ -9,7 +7,5 @@ rofi \
   -title true \
   -switchers "window,run,drun,ssh,Workspaces:i3_switch_workspaces.sh" \
   -show ${SHOW:='run'} \
-  -width 30 \
-  -location 0 \
   -lines 8
 
--- t
+++ t
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+export PATH=$HOME/.nvm/versions/node/v21.7.1/bin:$HOME/node_modules/.bin:$PATH
+
 rtm add $*
 #echo $* | mail -s "$*" $(cat $HOME/.config/trello/gtd-addr.txt)
 #task add $*
