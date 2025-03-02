11fca3d3ab21c0905868b5551859c204b8baf1ea
Chris Scheller <schelcj@pobox.com>
2025-03-02 17:00:20 -0500

    setup paths and such

--- rofi.sh
+++ rofi.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-export PATH=$HOME/scripts:$PATH
+export PATH=$HOME/scripts:$HOME/bin:$HOME/.config/emacs/bin:$PATH
 
-export PATH=$HOME/scripts:$HOME/bin:$PATH
 SHOW=$1
 
 rofi \
@@ -9,7 +8,6 @@ rofi \
   -title true \
   -switchers "window,run,drun,ssh,Workspaces:i3_switch_workspaces.sh" \
   -show ${SHOW:='run'} \
-  -width 30 \
   -location 0 \
   -lines 8
 
