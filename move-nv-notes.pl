f04f64f1abfd69089205a6537013a7a09f7c159a
Chris Scheller <schelcj@pobox.com>
2025-03-08 09:08:09 -0500

    first pass at migrating all the notes work.
    still need to review everything but this is close. not
    sure about the timestamps yet and might move them to silos.

--- move-nv-notes.pl
+++ move-nv-notes.pl
@@ -5,19 +5,55 @@ use Data::Dumper;
 use File::Basename;
 use File::Stat::OO;
 use IO::All;
+use Mojo::Template;
 
-my @notes = io->dir('/home/schelcj/Dropbox/Notes')->all;
+my $template = do { local $/; <DATA> };
+my @notes    = io->dir('/home/schelcj/Dropbox/Notes')->all;
 
 for my $note (@notes) {
+  print 'migrating ' . $note->name;
+
+  my $mt   = Mojo::Template->new();
   my $stat = File::Stat::OO->new({use_datetime => 1});
+  my $content = $note->slurp;
+
   $stat->stat($note->name);
 
-  my $date   = $stat->mtime->ymd('') . 'T' . $stat->mtime->hms('');
-  my ($name) = fileparse($note->name);
-  my @lines  = $note->getlines;
+  my $mtime      = $stat->mtime;
+  my $date_ident = $mtime->ymd('') . 'T' . $mtime->hms('');
+  my ($filename, $dirs, $suffix) = fileparse($note->name, qr/\.[^.]*/);
 
-  if ($lines[0] =~ /^#+TITLE: (.*)$)/) {
-  }
+  my @tags = do {
+    my @lines = $note->getlines;
+    if ($lines[1] =~ /^@/) {
+      map {s/@//rg} split(/\s/, $lines[1]);
+    } else {
+      ();
+    }
+  };
 
-  print Dumper sprintf '%s--%s__%s.%s', $date, $name, 'foo', $note->ext;
+  push @tags, (qw(nv vimpad migrated));
+
+  my $denote_filename = sprintf '%s--%s__%s.%s', $date_ident, $filename, join('_', @tags), $note->ext;
+  my $params          = {
+    title      => $filename,
+    date       => join(' ', $mtime->ymd('-'), $mtime->day_abbr, $mtime->hour_12_0 . ':' . $mtime->second),
+    date_ident => $date_ident,
+    tags       => join(':', @tags),
+    content    => $content,
+  };
+
+  my $new_note_contents = $mt->vars(1)->render($template, $params);
+
+  $new_note_contents > io('/home/schelcj/MigratedNotes/' . $denote_filename);
+
+  say ' - done';
 }
+
+__DATA__
+#+title: <%= $title %>
+##+date: [<%= $date %>]
+##+filetags: :<%= $tags %>:
+##+identifier: <%= $date_ident %>
+
+<%= $content %>
